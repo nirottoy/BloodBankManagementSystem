655b8a3e29fa4ed172e3fefc7946e060a6c49c44
Saha Junior Samay <18-37168-1@student.aiub.edu>
2021-01-01 17:01:51 +0600

    Resolving App.config duplicate issue

---
+++ Business Logic Layer/DonorService.cs
@@ -0,0 +1,28 @@
+﻿using BloodBankManagementSystem.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodBankManagementSystem.Business_Logic_Layer
+{
+    class DonorService
+    {
+        public int AddNewDoners(string dnName, string dnGender, int dnAge, int dnWeight, string dnPulse, int dnHimoglobin, string dnSkin, string dnHealthissue, string dnRhfactor, string dnBloodgroup, DateTime dnLastdate)
+        {
+            Donor doner = new Donor()
+            doner.Name = dnName;
+            doner.Gender = dnGender;
+            doner.Age = dnAge;
+            doner.Weight = dnWeight;
+            doner.TempPulse = dnPulse;
+            doner.Haemoglobin = dnHimoglobin;
+            doner.SkinDisease = dnSkin;
+            doner.MajorHealthIssue = dnHealthissue;
+            doner.BloodGroup = dnBloodgroup;
+            doner.RhFactor = dnRhfactor;
+            doner.LastDonatedDate = dnLastdate;
+        }
+    }
+}
