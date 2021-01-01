ed816f3aad4e2d23bc45169c251bd9705954effa
Saha Junior Samay <nirottoyxd@gmail.com>
2021-01-01 19:22:45 +0600

    deleted unnecessary files

--- Business Logic Layer/DonorService.cs
+++ Business Logic Layer/DonorService.cs
@@ -9,20 +9,20 @@ namespace BloodBankManagementSystem.Business_Logic_Layer
 {
     class DonorService
     {
-        public int AddNewDoners(string dnName, string dnGender, int dnAge, int dnWeight, string dnPulse, int dnHimoglobin, string dnSkin, string dnHealthissue, string dnRhfactor, string dnBloodgroup, DateTime dnLastdate)
-        {
-            Donor doner = new Donor()
-            doner.Name = dnName;
-            doner.Gender = dnGender;
-            doner.Age = dnAge;
-            doner.Weight = dnWeight;
-            doner.TempPulse = dnPulse;
-            doner.Haemoglobin = dnHimoglobin;
-            doner.SkinDisease = dnSkin;
-            doner.MajorHealthIssue = dnHealthissue;
-            doner.BloodGroup = dnBloodgroup;
-            doner.RhFactor = dnRhfactor;
-            doner.LastDonatedDate = dnLastdate;
-        }
+        //public int AddNewDoners(string dnName, string dnGender, int dnAge, int dnWeight, string dnPulse, int dnHimoglobin, string dnSkin, string dnHealthissue, string dnRhfactor, string dnBloodgroup, DateTime dnLastdate)
+        //{
+        //    Donor doner = new Donor()
+        //    doner.Name = dnName;
+        //    doner.Gender = dnGender;
+        //    doner.Age = dnAge;
+        //    doner.Weight = dnWeight;
+        //    doner.TempPulse = dnPulse;
+        //    doner.Haemoglobin = dnHimoglobin;
+        //    doner.SkinDisease = dnSkin;
+        //    doner.MajorHealthIssue = dnHealthissue;
+        //    doner.BloodGroup = dnBloodgroup;
+        //    doner.RhFactor = dnRhfactor;
+        //    doner.LastDonatedDate = dnLastdate;
+        //}
     }
 }
