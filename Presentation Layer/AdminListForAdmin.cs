a53c1e7815dd2c05cbb7632e34f80b5c06f287fb
17-35494-3@student.aiub.edu <17-35494-3@student.aiub.edu>
2021-01-04 00:27:25 +0600

    modified daLayer & entities

--- Presentation Layer/AdminListForAdmin.cs
+++ Presentation Layer/AdminListForAdmin.cs
@@ -1,12 +1,5 @@
 ﻿using BloodBankManagementSystem.Business_Logic_Layer;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BloodBankManagementSystem.Presentation_Layer
